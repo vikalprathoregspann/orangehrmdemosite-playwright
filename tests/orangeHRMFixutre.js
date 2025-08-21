531ead1d6df25d11a572ef1a89dbca5a8437eec0
Prince Raj <prince.raj@gspann.com>
2025-08-21 17:15:07 +0530

    Initial commit for hrmadminpage

--- tests/orangeHRMFixutre.js
+++ tests/orangeHRMFixutre.js
@@ -1,5 +1,6 @@
 import { test as base, expect } from "@playwright/test";
 import { HrmLoginPage } from "../pages/loginPageHRM";
+import { HrmAdminPage } from "../pages/adminPageHRM";
 
 export const test = base.extend({
   hrmLoginPage: async({page},use)=>{
@@ -14,6 +15,33 @@ export const test = base.extend({
   },
   hrmLaunchSetUp: async({hrmLoginPage},use)=>{
     await hrmLoginPage.launchTheApp()
+    await use()
+  },
+  hrmAdminPage: async({page},use)=>{
+    await use(new HrmAdminPage(page));
+  },
+  hrmAdminSetUp: async({hrmLoginSetUp ,hrmAdminPage},use)=>{
+    await hrmAdminPage.clickOnPimAdmBtn()
+    await hrmAdminPage.clickOnPimAddBtn()
+    await hrmAdminPage.enterPimUserFirstName("Suraj")
+    await hrmAdminPage.enterPimUserLastName("Raj")
+    await hrmAdminPage.enterPimEmployeeId("321987")
+    await hrmAdminPage.clickOnPimSaveBtn()
+    await hrmAdminPage.clickOnAdminBtn()
+    await hrmAdminPage.clickOnAddBtn()
+    await hrmAdminPage.enterUserRole()
+    await hrmAdminPage.enterStatus()
+    await hrmAdminPage.enterEmployeeName("Suraj Raj")
+    await hrmAdminPage.enterUserName("Vikalp KrGspann")
+    await hrmAdminPage.enterPassword("prince@12345")
+    await hrmAdminPage.enterConfirmPassword("prince@12345")
+    await use()
+  },
+  hrmAdminSearch: async({hrmLoginSetUp, hrmAdminPage},use)=>{
+    await hrmAdminPage.clickOnAdminBtn()
+    await hrmAdminPage.enterSearchUserName("Vikalp KrGspann")
+    await hrmAdminPage.enterSearchUserRole()
+    await hrmAdminPage.enterSearchEmployee("Suraj Raj")
     await use()
   }
 });
