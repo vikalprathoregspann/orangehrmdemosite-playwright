0d75cf2884267b076dc1ab08dd4b8cc1b6afcd48
Vikalp.rathore <vikalp.rathore@gspann>
2025-08-21 19:08:18 +0530

    Jenkins configuration changes in config

--- playwright.config.js
+++ playwright.config.js
@@ -16,13 +16,13 @@ export default defineConfig({
   timeout:60000,
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 2,
+  workers: process.env.CI ? 4 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
