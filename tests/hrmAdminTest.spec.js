a791680cdafe55d7fd89abf16edb2a6510374945
Prince Raj <prince.raj@gspann.com>
2025-08-21 17:43:37 +0530

    Added two test cases for Search Section of hrmAdminPage

--- tests/hrmAdminTest.spec.js
+++ tests/hrmAdminTest.spec.js
@@ -2,7 +2,6 @@ import { expect,test } from "./orangeHRMFixutre";
 
 test("adding data using save button @adminTest1",async({hrmAdminSetUp, hrmAdminPage})=>{
     await hrmAdminPage.clickOnSaveBtn()
-    // await expect(hrmAdminPage.isNewRecordVisible()).toBeTruthy()
     expect(await hrmAdminPage.isUserManagementTextVisible()).toBeFalsy();
 })
 
@@ -11,4 +10,15 @@ test("clicking on cancel button @adminTest2", async ({ hrmAdminSetUp, hrmAdminPa
     expect(await hrmAdminPage.isUserManagementTextVisible()).toBeTruthy();
 });
 
+test("search using valid data @adminTest3",async({hrmAdminSearch, hrmAdminPage})=>{
+    await hrmAdminPage.enterSearchStatus()
+    await hrmAdminPage.clickOnSearchBtn()
+    expect(await hrmAdminPage.isRecordAvailable()).toContain("(1) Record Found")
+})
+
+test("search using Invalid data @adminTest4",async({hrmAdminSearch, hrmAdminPage})=>{
+    await hrmAdminPage.enterSearchOtherStatus()
+    await hrmAdminPage.clickOnSearchBtn()
+    expect(await hrmAdminPage.isRecordAvailable()).toContain("No Records Found")
+})
 
