30eeb7b689b6eab27a929ae8ac46c0e74a49b724
Vikalp.rathore <vikalp.rathore@gspann>
2025-08-25 08:27:28 +0530

    Changes for Allure report in config

--- playwright.config.js
+++ playwright.config.js
@@ -22,10 +22,13 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : 2,
+  workers: process.env.CI ? 4 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+    // reporter: 'html',
+
+  reporter: [["line"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
